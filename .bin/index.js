ada03c693873043c27dc48b914a6214941039c0d
Edmund Dantes <d.ajaykumar.017@gmail.com>
2019-08-23 14:43:59 +0530

    Intial commit.
    Make definition look good.

---
+++ .bin/index.js
@@ -0,0 +1,113 @@
+const util = require("util");
+const chalk = require('chalk');
+
+const {
+    getWordOfTheDay,
+    getSynonyms,
+    getAntonyms,
+    getExamples,
+    getDefinition,
+    getDictionaryEntry,
+    isWord
+} = require("../lib");
+const playGame = require('../lib/play');
+
+function printUsage() {
+    console.log(`Usage: ./dict <verb|word> <word>`);
+    console.log(`verb = def|syn|ant|ex|dict|play`);
+    console.log(`word = a word of the english language`);
+    process.exit(1);
+}
+
+function parseArgs(args = process.argv) {
+    var verb, word;
+    const arguments = process.argv.slice(2);
+
+    if (
+        arguments.length === 2 &&
+        arguments[0] === "def" &&
+        isWord(arguments[1])
+    ) {
+        verb = "def";
+        word = arguments[1];
+    } else if (
+        arguments.length === 2 &&
+        arguments[0] === "syn" &&
+        isWord(arguments[1])
+    ) {
+        verb = "syn";
+        word = arguments[1];
+    } else if (
+        arguments.length === 2 &&
+        arguments[0] === "ant" &&
+        isWord(arguments[1])
+    ) {
+        verb = "ant";
+        word = arguments[1];
+    } else if (
+        arguments.length === 2 &&
+        arguments[0] === "ex" &&
+        isWord(arguments[1])
+    ) {
+        verb = "ex";
+        word = arguments[1];
+    } else if (
+        arguments.length === 2 &&
+        arguments[0] === "dict" &&
+        isWord(arguments[1])
+    ) {
+        verb = "dict";
+        word = arguments[1];
+    } else if (arguments.length === 1 && arguments[0] === "play") {
+        verb = "play";
+        word = null;
+    } else if (arguments.length === 1 && isWord(arguments[0])) {
+        verb = "dict";
+        word = arguments[0];
+    } else if (arguments.length === 0) {
+        verb = "wod";
+        word = null;
+    } else {
+        verb = "puse";
+        word = null;
+    }
+
+    return { verb, word };
+}
+
+(async function () {
+    const { verb, word } = parseArgs();
+
+    switch (verb) {
+        case "def":
+            const definitions = await getDefinition(word);
+            console.log(chalk.bold('\n' + word + '\n'));
+            console.log(definitions[0].text, '\n');
+            break;
+        case "syn":
+            await getSynonyms(word);
+            break;
+        case "ant":
+            await getAntonyms(word);
+            break;
+        case "ex":
+            await getExamples(word);
+            break;
+        case "dict":
+            await getDictionaryEntry(word);
+            break;
+        case "wod":
+            const todaysWord = await getWordOfTheDay();
+            await getDictionaryEntry(todaysWord);
+            break;
+        case "play":
+            await playGame();
+            break;
+        case "puse":
+        default:
+            printUsage();
+            break;
+    }
+
+    process.exit(0);
+})();
